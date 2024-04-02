b6c8b4e88fb2c27909a3025215a807551f27bbc3
Hersey <helane1996@gmail.com>
2024-04-02 15:13:28 -0400

    added a function to generate a task id based on time prints and started making the task cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,23 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 const modal = document.getElementById(`myModal`);
-const btn = document.getElementById(`myBtn`);
+const addBtn = document.getElementById(`myBtn`);
 const closeModal = document.getElementsByClassName(`close`)[0];
+const toDo = document.getElementById(`todo-cards`);
+const inProgress = document.getElementById(`in-progress-cards`);
+const done = document.getElementById(`done-cards`);
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {}
+function generateTaskId() {
+  const taskId = `id` + new Date().getTime();
+}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  const card = $(`<div class="card"></div>`);
+  const title = $(`<h2 class="card-header"></h2>`);
+  const info = $(`<div class="card-body"><`)
+}
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {}
